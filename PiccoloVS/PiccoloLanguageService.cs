0f93732891299f46b32de3ccc1e225a28b2ed085
MarcasRealAccount <thetremine.lumicks@gmail.com>
2021-11-23 23:45:39 +0100

    Initial Commit

---
+++ PiccoloVS/PiccoloLanguageService.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Outlining;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+
+namespace PiccoloVS
+{
+    class PartialRegion
+    {
+        public int Level { get; set; }
+        public int StartLine { get; set; }
+        public int StartOffset { get; set; }
+        public PartialRegion PartialParent { get; set; }
+    }
+
+    class Region : PartialRegion
+    {
+        public int EndLine { get; set; }
+    }
+
+    internal static class PiccoloLanguageService
+    {
+        [Export]
+        [Name("Piccolo")]
+        [BaseDefinition("code")]
+        internal static ContentTypeDefinition PiccoloTypeDefinition = new ContentTypeDefinition();
+
+        [Export]
+        [FileExtension(".pic")]
+        [ContentType("Piccolo")]
+        internal static FileExtensionToContentTypeDefinition PiccoloFileExtensionDefinition = new FileExtensionToContentTypeDefinition();
+    }
+
+    internal sealed class OutliningTagger : ITagger<IOutliningRegionTag>
+    {
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+        private string startHide = "{";
+        private string endHide = "}";
+        private string ellipsis = "...";
+        ITextBuffer buffer;
+        ITextSnapshot snapshot;
+        List<Region> regions;
+
+        public OutliningTagger(ITextBuffer buffer)
+        {
+            this.buffer = buffer;
+            this.snapshot = buffer.CurrentSnapshot;
+            this.regions = new List<Region>();
+            ReParse();
+            this.buffer.Changed += BufferChanged;
+        }
+
+        public IEnumerable<ITagSpan<IOutliningRegionTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            if (spans.Count == 0)
+                yield break;
+
+            List<Region> currentRegions = this.regions;
+            ITextSnapshot currentSnapshot = this.snapshot;
+            SnapshotSpan entire = new SnapshotSpan(spans[0].Start, spans[spans.Count - 1].End).TranslateTo(currentSnapshot, SpanTrackingMode.EdgeExclusive);
+            int startLineNumber = entire.Start.GetContainingLine().LineNumber;
+            int endLineNumber = entire.End.GetContainingLine().LineNumber;
+            foreach (var region in currentRegions)
+            {
+                if (region.StartLine <= endLineNumber && region.EndLine >= startLineNumber)
+                {
+                    var startLine = currentSnapshot.GetLineFromLineNumber(region.StartLine);
+                    var endLine = currentSnapshot.GetLineFromLineNumber(region.EndLine);
+                    SnapshotSpan span = new SnapshotSpan(startLine.Start + region.StartOffset, endLine.End);
+                    yield return new TagSpan<IOutliningRegionTag>(span, new OutliningRegionTag(false, false, ellipsis, currentSnapshot.GetText(span)));
+                }
+            }
+        }
+
+        void BufferChanged(object sender, TextContentChangedEventArgs e)
+        {
+            if (e.After != buffer.CurrentSnapshot)
+                return;
+            ReParse();
+        }
+
+        void ReParse()
+        {
+            ITextSnapshot newSnapshot = buffer.CurrentSnapshot;
+            List<Region> newRegions = new List<Region>();
+
+            PartialRegion currentRegion = null;
+
+            foreach (var line in newSnapshot.Lines)
+            {
+                int regionStart = -1;
+                string text = line.GetText();
+
+                if ((regionStart = text.IndexOf(startHide, StringComparison.Ordinal)) != -1)
+                {
+                    int currentLevel = (currentRegion != null) ? currentRegion.Level : 1;
+                    int newLevel;
+                    if (!TryGetLevel(text, regionStart, out newLevel))
+                        newLevel = currentLevel + 1;
+
+                    if (currentLevel == newLevel && currentRegion != null)
+                    {
+                        newRegions.Add(new Region()
+                        {
+                            Level = currentRegion.Level,
+                            StartLine = currentRegion.StartLine,
+                            StartOffset = currentRegion.StartOffset,
+                            EndLine = line.LineNumber
+                        });
+
+                        currentRegion = new PartialRegion()
+                        {
+                            Level = newLevel,
+                            StartLine = line.LineNumber,
+                            StartOffset = regionStart,
+                            PartialParent = currentRegion.PartialParent
+                        };
+                    }
+                    else
+                    {
+                        currentRegion = new PartialRegion()
+                        {
+                            Level = newLevel,
+                            StartLine = line.LineNumber,
+                            StartOffset = regionStart,
+                            PartialParent = currentRegion
+                        };
+                    }
+                }
+                else if ((regionStart = text.IndexOf(endHide, StringComparison.Ordinal)) != -1)
+                {
+                    int currentLevel = (currentRegion != null) ? currentRegion.Level : 1;
+                    int closingLevel;
+                    if (!TryGetLevel(text, regionStart, out closingLevel))
+                        closingLevel = currentLevel;
+
+                    if (currentRegion != null && currentLevel == closingLevel)
+                    {
+                        newRegions.Add(new Region()
+                        {
+                            Level = currentLevel,
+                            StartLine = currentRegion.StartLine,
+                            StartOffset = currentRegion.StartOffset,
+                            EndLine = line.LineNumber
+                        });
+
+                        currentRegion = currentRegion.PartialParent;
+                    }
+                }
+            }
+
+            List<Span> oldSpans = new List<Span>(this.regions.Select(r => AsSnapshotSpan(r, this.snapshot).TranslateTo(newSnapshot, SpanTrackingMode.EdgeExclusive).Span));
+            List<Span> newSpans = new List<Span>(newRegions.Select(r => AsSnapshotSpan(r, newSnapshot).Span));
+
+            NormalizedSpanCollection oldSpanCollection = new NormalizedSpanCollection(oldSpans);
+            NormalizedSpanCollection newSpanCollection = new NormalizedSpanCollection(newSpans);
+
+            NormalizedSpanCollection removed = NormalizedSpanCollection.Difference(oldSpanCollection, newSpanCollection);
+
+            int changeStart = int.MaxValue;
+            int changeEnd = -1;
+
+            if (removed.Count > 0)
+            {
+                changeStart = removed[0].Start;
+                changeEnd = removed[removed.Count - 1].End;
+            }
+
+            if (newSpans.Count > 0)
+            {
+                changeStart = Math.Min(changeStart, newSpans[0].Start);
+                changeEnd = Math.Max(changeEnd, newSpans[newSpans.Count - 1].End);
+            }
+
+            this.snapshot = newSnapshot;
+            this.regions = newRegions;
+
+            if (changeStart <= changeEnd)
+            {
+                ITextSnapshot snap = this.snapshot;
+                if (this.TagsChanged != null)
+                    this.TagsChanged(this, new SnapshotSpanEventArgs(new SnapshotSpan(this.snapshot, Span.FromBounds(changeStart, changeEnd))));
+            }
+        }
+
+        static bool TryGetLevel(string text, int startIndex, out int level)
+        {
+            level = -1;
+            if (text.Length > startIndex + 3 && int.TryParse(text.Substring(startIndex + 1), out level))
+                return true;
+            return false;
+        }
+
+        static SnapshotSpan AsSnapshotSpan(Region region, ITextSnapshot snapshot)
+        {
+            var startLine = snapshot.GetLineFromLineNumber(region.StartLine);
+            var endLine = (region.StartLine == region.EndLine) ? startLine : snapshot.GetLineFromLineNumber(region.EndLine);
+            return new SnapshotSpan(startLine.Start + region.StartOffset, endLine.End);
+        }
+    }
+
+    [Export(typeof(ITaggerProvider))]
+    [TagType(typeof(IOutliningRegionTag))]
+    [ContentType("Piccolo")]
+    internal sealed class OutliningTaggerProvider : ITaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            Func<ITagger<T>> sc = delegate () { return new OutliningTagger(buffer) as ITagger<T>; };
+            return buffer.Properties.GetOrCreateSingletonProperty<ITagger<T>>(sc);
+        }
+    }
+}
---
+++ PiccoloVS/PiccoloPackage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace PiccoloVS
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(PiccoloPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class PiccoloPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "10b12f57-ad3d-49d6-bd23-2bcfcf25461f";
+
+        public PiccoloPackage()
+        {
+        }
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
